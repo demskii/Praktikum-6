7ceb15d696aca67461b52846b53749606e4d0701
demskii <109432905+demskii@users.noreply.github.com>
2022-11-28 23:31:06 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,16 @@
+public abstract class BangunDatar {
+    String warna;
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getWarna() {
+        return this.warna;
+    }
+
+    public abstract void gambar();
+
+    public abstract float getLuas();
+
+}
